bc1303bdd9c58ceeb175a1f6591835bf34750c8d
Amaury Blanchet <amblanch@2G6.42angouleme.fr>
2025-06-27 16:09:04 +0200

    feat: C06

---
+++ ex03/ft_sort_params.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amblanch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/19 11:41:57 by amblanch          #+#    #+#             */
+/*   Updated: 2024/07/19 11:58:15 by amblanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	(void) ac;
+	i = 1;
+	while (av[i])
+	{
+		j = i + 1;
+		while (av[j])
+		{
+			if (ft_strcmp(av[i], av[j]) > 0)
+				ft_swap(&av[i], &av[j]);
+			j++;
+		}
+		k = 0;
+		while (av[i][k])
+		{
+			write(1, &av[i][k], 1);
+			k++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+}
